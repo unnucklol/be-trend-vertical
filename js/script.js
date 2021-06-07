14c9a18a4057e9721e58c1a0f5f51004a0947802
unnucklol <65903820+unnucklol@users.noreply.github.com>
2021-06-07 22:50:53 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -111,3 +111,141 @@ if (menuLinks.length > 0) {
 
 */
 // --------------------------------------------------------------------------------
+// Popup скрипт--------------------------------------------------------------------
+
+const popupLinks = document.querySelectorAll('.popup-link');
+const body = document.querySelector('body');
+const lockPadding = document.querySelectorAll('.lock-padding');
+
+let unlock = true;
+
+// Та же самая цифра, которая в transition появления попапа
+const timeout = 1000;
+
+if (popupLinks.length > 0) {
+    for (let index = 0; index < popupLinks.length; index++) {
+        const popupLink = popupLinks[index];
+        popupLink.addEventListener('touchstart', function(e) {
+            const popupName = popupLink.getAttribute('href').replace('#', '');
+            const currentPopup = document.getElementById(popupName);
+            popupOpen(currentPopup);
+            e.preventDefault();
+        });
+    }
+}
+
+const popupCloseIcon = document.querySelectorAll('.close-popup');
+if (popupCloseIcon.length > 0) {
+    for (let index = 0; index < popupCloseIcon.length; index++) {
+        const el = popupCloseIcon[index];
+        el.addEventListener('touchstart', function(e) {
+            popupClose(el.closest('.popup'));
+            e.preventDefault();
+        })
+    }
+}
+
+function popupOpen(currentPopup) {
+    if (currentPopup && unlock) {
+        
+        // для случаев если есть попап в попапе (закрывает первый)
+        const popupActive = document.querySelector('.popup.open');
+        if (popupActive) {
+            popupClose(popupActive, false);
+        } else {
+            bodyLock();
+        }
+        // --------------------------------------------------------
+        currentPopup.classList.add('open');
+        currentPopup.addEventListener('touchstart', function (e) {
+            if (!e.target.closest('.popup__content')) {
+                popupClose(e.target.closest('.popup'));
+            }
+        });
+    }
+}
+
+
+function popupClose(popupActive, doUnlock = true) {
+    if (unlock) {
+        // для проверки, если открывается попап в попапе
+        popupActive.classList.remove('open');
+        if (doUnlock) {
+            bodyUnlock();
+        }
+    }
+}
+
+// убирает скролл + добавляет паддинги фиксированным элементам (.lock-padding)
+function bodyLock() {
+    const lockPaddingValue = window.innerWidth - document.querySelector('.page-wrapper').offsetWidth + 'px';
+    if (lockPadding.length > 0) {
+
+        for (let index = 0; index < lockPadding.length; index++) {
+            const el = lockPadding[index];
+            console.log(el);
+            el.style.paddingRight = lockPaddingValue;
+        }
+    }
+    body.style.paddingRight = lockPaddingValue;
+    body.classList.add('lock');
+
+    unlock = false;
+    setTimeout(function () {
+        unlock = true;        
+    }, timeout);
+    
+}
+
+function bodyUnlock() {
+    setTimeout(function () {
+        if (lockPadding) {
+            for (let index = 0; index < lockPadding.length; index++) {
+                const el = lockPadding[index];
+                el.style.paddingRight = '0px';
+            }
+        }
+        body.style.paddingRight = '0px';
+        body.classList.remove('lock');
+    }, timeout);
+
+    unlock = false;
+    setTimeout(function () {
+        unlock = true;
+    }, timeout);
+
+}
+
+document.addEventListener('keydown', function (e) {
+    if (e.which === 27) {
+        const popupActive = document.querySelector('.popup.open');
+        popupClose(popupActive);
+    }
+});
+
+// Определяем метод для браузеров, которые не поддерживают их
+(function () {
+    //проверяем поддержку
+    if (!Element.prototype.closest) {
+        //реализуем данный метод
+        Element.prototype.closest = function (css) {
+            var node = this;
+            while (node) {
+                if (node.matches(css)) return node;
+                else node = node.parentElement;
+            }
+            return null;
+        };
+    }
+})();
+
+(function () {
+    // проверяем поддержку
+    if (!Element.prototype.matches) {
+        //определяем свойство(метод)
+        Element.prototype.matches = Element.prototype.matchesSelector ||
+            Element.prototype.webkitMatchesSelector ||
+            Element.prototype.mozMatchesSelector ||
+            Element.prototype.msMatchesSelector;
+    }
+})();
